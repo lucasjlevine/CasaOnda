e43b5f74e0bb601e66b98ae7caedbd03fca4b345
Lucas Levine <lucaslevine12@gmail.com>
2025-06-14 18:33:58 +0200

    1.0

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import "../app.css";
+
+  let { children } = $props();
+</script>
+
+<main class="newsreader">
+  {@render children()}
+</main>
+
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Newsreader:ital,opsz,wght@0,6..72,200..800;1,6..72,200..800&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
